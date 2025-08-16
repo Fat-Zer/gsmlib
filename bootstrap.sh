b48f0ab40332b8b034b5f49ed305fa151b378e6f
Alexander Golubev <fatzer2@gmail.com>
2025-08-16 15:05:42 +0300

    Update autotools scripts
    * get rid of bundled libintl (autotools drop support for it)
    * add autopoint to bootstrap
    * run autoupdate at accept in changes to configure.ac
    * rename {INCLUDES -> AM_CPPFLAGS} in Makefile.am`s
    * remove more autotools-generated files
    * remove acconfig.h as it doesn't seem to be needed anymore

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 
 # generate ./configure and other autotools files
 libtoolize --install --copy --force --automake
+autopoint --force
 aclocal
 autoconf --force
 autoheader --force
